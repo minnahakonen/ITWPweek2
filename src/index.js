3002386c22d3ab50f369051c219978f07c1be24a
minnahakonen <101180642+minnahakonen@users.noreply.github.com>
2022-09-15 19:17:28 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,78 @@
+import "./styles.css";
+
+const submitButton = document.getElementById("submit-data");
+
+//const listofusers = ["Admin-na", "User1", "User2"];
+
+submitButton.addEventListener("click", function () {
+  const tableBody = document.getElementById("table-body");
+  const Un = document.createElement("td");
+  const Em = document.createElement("td");
+  const Adr = document.createElement("td");
+  const Amn = document.createElement("td");
+  const Pic = document.createElement("td");
+
+  const row = document.createElement("tr");
+
+  Un.innerHTML = document.getElementById("input-username").value;
+  /*const testun = Un.textContent;
+  console.log(testun);
+  for (let i = 0; i < listofusers.length; i++) {
+    console.log(i);
+    if (listofusers[i] === testun) {
+      console.log("value is same: " + listofusers[i]);
+    } else {
+      console.log("value is not the same");
+      listofusers.push(Un.textContent);
+    }*/
+
+  //console.log(listofusers);
+  Em.innerHTML = document.getElementById("input-email").value;
+  Adr.innerHTML = document.getElementById("input-address").value;
+
+  if (document.getElementById("input-admin").checked) {
+    Amn.innerHTML = "X";
+  } else {
+    Amn.innerHTML = "-";
+  }
+
+  //const imageButton = document.getElementById("input-image");
+  //imageButton.addEventListener("click", () => {});
+  const file = document.getElementById("input-image").files[0];
+  const newimage = document.createElement("img");
+  newimage.src = URL.createObjectURL(file);
+  newimage.height = 64;
+  newimage.width = 64;
+
+  Pic.appendChild(newimage);
+
+  //Pic.innerHTML = document.getElementById("input-image").value;
+
+  row.appendChild(Un);
+  row.appendChild(Em);
+  row.appendChild(Adr);
+  row.appendChild(Amn);
+  row.appendChild(Pic);
+
+  tableBody.appendChild(row);
+
+  /*const user = document.getElementById("input-username").value;
+  const allUsernames = document.querySelectorAll("#table-body")[0].textContent;
+  console.log("all usernames: " + allUsernames + "and the users" + user);
+
+  for (let i = 0; i < allUsernames.length; i++) {
+    if (allUsernames[i].value !== user.value) {
+    
+
+      
+    } else {
+      /*alert("The name already exist");
+    }
+  }*/
+});
+
+const emptyButton = document.getElementById("empty-table");
+
+emptyButton.addEventListener("click", function () {
+  document.getElementById("table-body").remove();
+});
